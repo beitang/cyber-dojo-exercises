daea38d3d20f2edf59276bfb0aac9078faf09267
beitang <beibeitang@gmail.com>
2014-11-29 19:43:33 +0800

    Code can parse one haiku now

--- Haiku/Haiku.py
+++ Haiku/Haiku.py
@@ -1,4 +1,32 @@
+def get_line_syllables(line):
+	syllables = 0
+	words = line.split(" ")
+	for word in words:
+		syllables += get_word_syllables(word)
+	return syllables
+
+def get_word_syllables(word):
+	syllables = 0
+	if is_syllable_letter(word[0]):
+		syllables += 1
+	for i in range(1, len(word)):
+		if is_syllable_letter(word[i]) and (not is_syllable_letter(word[i - 1])):
+			syllables += 1 
+	return syllables
+	
+def is_syllable_letter(letter):
+	if letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u' or letter == 'y':
+		return True
+	return False
+
 class Haiku:
 
-	def answer(self):
-		return 6 * 7
+	def output(self, string):
+		result = ""
+		lines = string.split('/')
+		for i in range(0, 3):
+			syllables = get_line_syllables(lines[i])
+			result += str(syllables) + ","
+		if result == "5,7,5,":
+			return result + "Yes\n"
+		return result + "No\n"
--- Haiku/Test_Haiku.py
+++ Haiku/Test_Haiku.py
@@ -3,9 +3,21 @@ import unittest
 
 class TestHaiku(unittest.TestCase):
 
-	def test_haiku(self):
-		douglas = Haiku.Haiku()
-		self.assertEqual(42, douglas.answer())
+	def test_one_haiku_one_word_each_line(self):
+		haiku = Haiku.Haiku()
+		self.assertEqual("1,1,1,No\n", haiku.output("work/work/work"))
+
+	def test_one_haiku_two_words_each_line(self):
+		haiku = Haiku.Haiku()
+		self.assertEqual("2,2,2,No\n", haiku.output("work day/work day/work day"))
+
+	def test_one_haiku_is_yes_each_word_with_one_syllable(self):
+		haiku = Haiku.Haiku()
+		self.assertEqual("5,7,5,Yes\n", haiku.output("a work day with work/a work day with no work now/a work day with work"))
+
+	def test_one_haiku_one_word_different_syllable(self):
+		haiku = Haiku.Haiku()
+		self.assertEqual("1,2,3,No\n", haiku.output("work/happy/different"))
 
 if __name__ == '__main__':
 	unittest.main()
